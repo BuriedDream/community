b5da2428ef604ba0906e61c476a2111d38c4f1d4
BuriedDream <xfmdenw@163.com>
2019-08-27 15:42:26 +0800

    初始化

---
+++ community/src/main/java/edu/jxau/community/entity/User.java
@@ -0,0 +1,12 @@
+package edu.jxau.community.entity;
+
+/**
+ * @title: community
+ * @ClassName User.java
+ * @Description:
+ * @Author: liam
+ * @Date: 2019/8/27
+ * @Version: 1.0
+ **/
+public class User {
+}
